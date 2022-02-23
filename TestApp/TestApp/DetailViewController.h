e20d8dd6b3a47bfe6666f1796d1a9f1af939aaf4
Rostadhi <rostadhi@gmail.com>
2022-02-23 08:30:37 +0700

    +

---
+++ TestApp/TestApp/DetailViewController.h
@@ -0,0 +1,16 @@
+//
+//  DetailViewController.h
+//  TestApp
+//
+//  Created by Rostadhi Akbar, M.Pd on 23/02/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DetailViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TestApp/TestApp/DetailViewController.m
@@ -0,0 +1,24 @@
+//
+//  DetailViewController.m
+//  TestApp
+//
+//  Created by Rostadhi Akbar, M.Pd on 23/02/22.
+//
+
+#import "DetailViewController.h"
+
+@interface DetailViewController ()
+
+@end
+
+@implementation DetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+}
+
+
+
+@end
